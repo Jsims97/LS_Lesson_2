87511971bd48c4668528ab8f38173f6b665ce014
Jordan Sims <j,sims0997@gmail.com>
2022-09-24 17:47:18 -0400

    RPS4

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -6,14 +6,29 @@
 
 const RL = require('readline-sync');
 
-
-
 validChoices = ['rock', 'paper', 'scissors'];
 
 function prompt(msg){
   console.log(`=>${msg}`);
 }
 
+function displayWinner(userAnswer,computerAnswer){
+  prompt(`You chose ${userAnswer}. The computer chose ${computerAnswer}`)
+
+  if ((userAnswer === 'rock' && computerAnswer === 'scissors') ||
+      (userAnswer === 'scissors' &&  computerAnswer === 'paper') ||
+      (userAnswer === 'paper' && computerAnswer === 'rock')) {
+        prompt('You beat Jordan!');
+      } else if ((userAnswer === 'rock' && computerAnswer === 'paper') ||
+                (userAnswer === 'paper' && computerAnswer === 'scissors') ||
+                (userAnswer === 'scissors' && computerAnswer === 'rock')){
+        prompt('Jordan beat you!');
+      } else {
+        prompt('Its a tie!');
+      }
+}
+
+while (true) {
 prompt(`Welcome to Jordan's ${validChoices.join(', ')}!\nPlease choose: \nrock , paper, or scissors`);
 let userAnswer = RL.question().toLowerCase();
 
@@ -26,17 +41,14 @@ while (!['rock' , 'paper' , 'scissors'].includes(userAnswer)) {
 let randomIndex = Math.floor(Math.random() * validChoices.length);
 let computerAnswer = validChoices[randomIndex]
 
+displayWinner(userAnswer, computerAnswer);
 
-prompt(`You chose ${userAnswer}. The computer chose ${computerAnswer}`)
+prompt("Would you like to play again? \ny/n")
+let answer = RL.question().toLowerCase();
+while (answer[0] !== 'y' && answer[0] !== 'n') {
+  prompt('please enter y or n')
+  answer = RL.question().toLowerCase()
+}
 
-if ((userAnswer === 'rock' && computerAnswer === 'scissors') ||
-    (userAnswer === 'scissors' &&  computerAnswer === 'paper') ||
-    (userAnswer === 'paper' && computerAnswer === 'rock')) {
-      prompt('You beat Jordan!');
-    } else if ((userAnswer === 'rock' && computerAnswer === 'paper') ||
-              (userAnswer === 'paper' && computerAnswer === 'scissors') ||
-              (userAnswer === 'scissors' && computerAnswer === 'rock')){
-      prompt('Jordan beat you!');
-    } else {
-      prompt('Its a tie!');
-    }
+if (answer[0] !== 'y') break;
+}
