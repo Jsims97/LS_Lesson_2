0f74889df69052566bf308204af93c20efdc265d
Jordan Sims <j,sims0997@gmail.com>
2022-09-24 16:27:23 -0400

    RPS2

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -8,7 +8,7 @@ const RL = require('readline-sync');
 
 
 
-validChoices = ['Rock', 'Paper', 'Scissors']
+validChoices = ['Rock', 'Paper', 'Scissors'];
 
 function prompt(msg){
   console.log(`=>${msg}`);
@@ -18,15 +18,25 @@ prompt(`Welcome to Jordan's ${validChoices.join(', ')}!\nPlease choose: \nRock ,
 let userAnswer = RL.question();
 
 while (!['Rock' , 'Paper' , 'Scissors'].includes(userAnswer)) {
-  prompt(`Please choose: \nRock , Paper, or Scissors`)
+  prompt(`Please choose: \nRock , Paper, or Scissors`);
   userAnswer = RL.question();
 
 }
-
-
 
 let randomIndex = Math.floor(Math.random() * validChoices.length);
 let computerAnswer = validChoices[randomIndex]
 
 
 prompt(`You chose ${userAnswer}. The computer chose ${computerAnswer}`)
+
+if ((userAnswer === 'Rock' && computerAnswer === 'Scissors') ||
+    (userAnswer === 'Scissors' &&  computerAnswer === 'Paper') ||
+    (userAnswer === 'Paper' && computerAnswer === 'Rock')) {
+      prompt('You beat Jordan!');
+    } else if ((userAnswer === 'Rock' && computerAnswer === 'Paper') ||
+              (userAnswer === 'Paper' && computerAnswer === 'Scissors') ||
+              (userAnswer === 'Scissors' && computerAnswer === 'Rock')){
+      prompt('Jordan beat you!');
+    } else {
+      prompt('Its a tie!');
+    }
