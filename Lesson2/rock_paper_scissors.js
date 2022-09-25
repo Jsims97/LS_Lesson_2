164e127069ea067bcf5d43f021d40d211a2ab16d
Jordan Sims <j,sims0997@gmail.com>
2022-09-24 21:00:31 -0400

    Cleanup

--- Lesson2/rock_paper_scissors.js
+++ Lesson2/rock_paper_scissors.js
@@ -1,16 +1,7 @@
-// Functions
-// Welcome Message
-// Get user input
-// Get computer input
-// Produce outcome
-
+// Functions & values
 const RL = require('readline-sync');
 
 validChoices = ['rock', 'paper', 'scissors'];
-
-function prompt(msg){
-  console.log(`=>${msg}`);
-}
 
 function displayWinner(userAnswer,computerAnswer){
   prompt(`You chose ${userAnswer}. The computer chose ${computerAnswer}`)
@@ -28,22 +19,34 @@ function displayWinner(userAnswer,computerAnswer){
       }
 }
 
+function prompt(msg){
+  console.log(`=>${msg}`);
+}
+
+
+//Run Program
 while (true) {
+
+//Welcome message and get input for users answer
 prompt(`Welcome to Jordan's ${validChoices.join(', ')}!\nPlease choose: \nrock , paper, or scissors`);
 let userAnswer = RL.question().toLowerCase();
 
+// if users input is not a valid input retry the question
 while (!['rock' , 'paper' , 'scissors'].includes(userAnswer)) {
   prompt(`Please choose: \nRock , Paper, or Scissors`);
   userAnswer = RL.question();
 
 }
 
+// Get computers answer
 let randomIndex = Math.floor(Math.random() * validChoices.length);
 let computerAnswer = validChoices[randomIndex]
 
+//Tell user the results of the game
 displayWinner(userAnswer, computerAnswer);
-
+//Ask user if they would like to play again
 prompt("Would you like to play again? \ny/n")
+
 let answer = RL.question().toLowerCase();
 while (answer[0] !== 'y' && answer[0] !== 'n') {
   prompt('please enter y or n')
@@ -52,3 +55,4 @@ while (answer[0] !== 'y' && answer[0] !== 'n') {
 
 if (answer[0] !== 'y') break;
 }
+//End of Program
